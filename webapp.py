9645a5288a8268cd31ade847f7d78a676dc4a373
Jan Skowronski <crianonim@gmail.com>
2023-11-01 14:17:12 +0000

    flask request

--- webapp.py
+++ webapp.py
@@ -1,6 +1,6 @@
 # TO RUN: python -m flask --app webapp run
 
-from flask import Flask
+from flask import Flask, request
 import monsters
 app = Flask(__name__)
 
@@ -12,8 +12,11 @@ def hello_world():
 
 @app.route("/monsters/<index>")
 def monster_route(index):
+    print(request.headers)
     m = list(filter(lambda m: m.index == index, monsters.monsters))[0]
-    return str(m)
+    return f'''<div>{str(m)}<div>
+    <div>{str(request.headers)}<div>
+    '''
 
 
 @app.route("/monsters")
