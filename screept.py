e813ce236093064b308e2759e4c87787a1af6632
Jan Skowronski <crianonim@gmail.com>
2023-11-03 14:34:14 +0000

    complete expression eval

--- screept.py
+++ screept.py
@@ -5,9 +5,10 @@ Screept
 Implements Screept "programming language" in python >3.12
 """
 from collections.abc import Mapping
+from copy import deepcopy
 from typing import Sequence
 
-from lark import Lark, Transformer, v_args
+from lark import Lark, Transformer, v_args, Token
 from dataclasses import dataclass
 
 
@@ -23,6 +24,20 @@ class LiteralValue(Value):
     pass
 
 
+class Identifier:
+    pass
+
+
+@dataclass
+class IdentifierLiteral(Identifier):
+    value: str
+
+
+@dataclass
+class IdentifierComputed(Identifier):
+    value: Expression
+
+
 @dataclass
 class Environment:
     vars: Mapping[str, LiteralValue]
@@ -30,7 +45,7 @@ class Environment:
 
 @dataclass
 class ExpressionVar(Expression):
-    identifier: str
+    identifier: Identifier
 
 
 @dataclass
@@ -44,9 +59,14 @@ class ValueString(LiteralValue):
 
 
 @dataclass
+class ValueFunction(Expression):
+    value: Expression
+
+
+@dataclass
 class FuncCall(Expression):
     identifier: str
-    values: Sequence[Value]
+    args: Sequence[Value]
 
 
 @dataclass
@@ -65,8 +85,8 @@ class UnaryOP(Expression):
 @dataclass
 class Conditional(Expression):
     cond: Expression
-    ifTrue: Expression
-    ifFalse: Expression
+    if_true: Expression
+    if_false: Expression
 
 
 @dataclass
@@ -83,7 +103,7 @@ calc_grammar = """
         | comparison
     
     ?comparison : sum
-        | sum "==" sum -> comp_equal
+        | sum "==" sum      -> comp_equal
     
     ?sum: product
         | sum "+" product   -> add
@@ -95,11 +115,15 @@ calc_grammar = """
 
     ?atom: NUMBER           -> number
          | "-" atom         -> neg
-         | NAME "(" [conditional ("," conditional)*] ")" -> func_call
-         | NAME             -> var
+         | "FUNC" conditional -> func
+         | identifier "(" [conditional ("," conditional)*] ")" -> func_call
+         | identifier             -> var
          | "(" conditional ")"
          | STRING           -> string
 
+    ?identifier : NAME 
+         | "$[" conditional "]" -> id_computed 
+    
     %import common.CNAME -> NAME
     %import common.NUMBER
     %import common.WS_INLINE
@@ -110,11 +134,18 @@ calc_grammar = """
 
 @v_args(inline=True)  # Affects the signatures of the methods
 class Ast(Transformer):
+    @staticmethod
+    def number(n):
+        return ValueNumber(float(n))
 
     @staticmethod
     def string(s):
         # Remove quotation marks
         return ValueString(s[1:-1])
+
+    @staticmethod
+    def func(n):
+        return ValueFunction(n)
 
     @staticmethod
     def var(name):
@@ -133,16 +164,20 @@ class Ast(Transformer):
         return BinaryOp(a, "-", b)
 
     @staticmethod
+    def NAME(a: Token) -> Identifier:
+        return IdentifierLiteral(a.value)
+
+    @staticmethod
+    def id_computed(expr: Expression) -> Identifier:
+        return IdentifierComputed(expr)
+
+    @staticmethod
     def mul(a, b):
         return BinaryOp(a, "*", b)
 
     @staticmethod
     def div(a, b):
         return BinaryOp(a, "/", b)
-
-    @staticmethod
-    def number(n):
-        return ValueNumber(float(n))
 
     @staticmethod
     def neg(n):
@@ -170,12 +205,20 @@ def main():
         print(calc2(s))
 
 
-def getLiteralValue(v: LiteralValue) -> str | float:
+def get_literal_value(v: LiteralValue) -> str | float:
     match v:
         case ValueNumber(x):
             return x
         case ValueString(x):
             return x
+
+
+def get_identifier_value(i: Identifier, env: Environment) -> str:
+    match i:
+        case IdentifierLiteral(x):
+            return x
+        case IdentifierComputed(x):
+            return str(evaluate_expression(x, env))
 
 
 def evaluate_expression(e: Expression, env: Environment):
@@ -199,19 +242,53 @@ def evaluate_expression(e: Expression, env: Environment):
                     raise Exception
             return binary(evaluate_expression(left, env), evaluate_expression(right, env))
         case ExpressionVar(i):
-            return getLiteralValue(env.vars[i])
+            return get_literal_value(env.vars[get_identifier_value(i, env)])
         case UnaryOP(left, op):
             match op:
                 case "-": return -evaluate_expression(left)
+        case FuncCall(i, args):
+            func = env.vars[get_identifier_value(i, env)]
+            if isinstance(func, ValueFunction):
+                new_env = environment_with_args(env, args)
+
+                return evaluate_expression(func.value, new_env)
+
+            raise Exception
+        case ComparisonEqual(left, right):
+            if evaluate_expression(left, env) == evaluate_expression(right, env):
+                return 1
+            else:
+                return 0
+        case Conditional(cond, if_true, if_false):
+            if cond == 0:
+                return evaluate_expression(if_false, env)
+            else:
+                return evaluate_expression(if_true, env)
+        case _:
+            print("DIDN'T handle", e)
+
+
+def environment_with_args(env: Environment, args: Sequence[Value]) -> Environment:
+    new_env = deepcopy(env)
+    for i, arg in enumerate(args):
+        new_env.vars['_' + str(i)] = arg
+    return new_env
 
 
 def test():
-    env: Environment = Environment({'abc': ValueNumber(66)})
+    env: Environment = Environment({'abc': ValueNumber(66)
+                                       , 'fun1': ValueFunction(
+            value=BinaryOp(left=ExpressionVar(IdentifierLiteral(value='_0')), op='+',
+                           right=ExpressionVar(identifier=IdentifierLiteral(value='_1'))))
+                                    })
     # print(calc2("a = 1+2"))
     # expr = "5-1+a*(-3-3)==3?1?3:2:4"
-    expr = "a(12,33)"
-
-    expr2 = 'abc+4'
+    # expr = """ $[ "a"+"b"+"c"] """
+    expr = """fun1(2,3)==5?1:0"""
+    print(expr)
+    # expr2 = 'abc+4'
+    # expr2 = """ $[ "a"+"b"+"c"] + 5 """
+    expr2 = """fun1(2,3)==5?1:0"""
     parsed3 = calc2(expr2)
     print("S", parsed3)
     print("EV", evaluate_expression(parsed3, env))
