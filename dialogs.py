09e11381d390d79180675a16c00c15cf4bf813f1
Jan Skowronski <crianonim@gmail.com>
2023-11-07 15:19:58 +0000

    partial parse statement

--- dialogs.py
+++ dialogs.py
@@ -4,7 +4,8 @@ from dataclasses import dataclass
 from pprint import pprint
 import screept
 
-def parse_value(d)->screept.Value:
+
+def parse_value(d) -> screept.Value:
     match (d):
 
         case {"type": "number", "value": x}:
@@ -13,10 +14,11 @@ def parse_value(d)->screept.Value:
             return screept.ValueString(x)
         case {"type": "func", "value": x}:
             return screept.ValueFunction(parse_expression(x))
-        case _ :
-            raise Exception("Can't parse Value",d)
+        case _:
+            raise Exception("Can't parse Value", d)
 
-def parse_identifier(d)->screept.Identifier:
+
+def parse_identifier(d) -> screept.Identifier:
     match (d):
         case {"type": 'literal', 'value': v}:
             return screept.IdentifierLiteral(v)
@@ -24,36 +26,59 @@ def parse_identifier(d)->screept.Identifier:
 
 def parse_expression(d):
     match (d):
-        case {'type':'binary_op','op':op,'x':left,'y':right}:
-            return screept.ExprBinaryOp(parse_expression(left),op,parse_expression(right))
-        case {'type': 'conditon', 'condition': expr, 'onFalse': on_false, 'onTrue':on_true}:
-            return screept.ExprConditional(parse_expression(expr),parse_expression(on_true),parse_expression(on_false))
-        case {'type':'var', 'identifier': identifier}:
+        case {'type': 'binary_op', 'op': op, 'x': left, 'y': right}:
+            return screept.ExprBinaryOp(parse_expression(left), op, parse_expression(right))
+        case {'type': 'conditon', 'condition': expr, 'onFalse': on_false, 'onTrue': on_true}:
+            return screept.ExprConditional(parse_expression(expr), parse_expression(on_true),
+                                           parse_expression(on_false))
+        case {'type': 'var', 'identifier': identifier}:
             return screept.ExpressionVar(parse_identifier(identifier))
-        case {'type':'literal','value':value}:
+        case {'type': 'literal', 'value': value}:
             return parse_value(value)
-        case {'type':'fun_call','identifier':identifier,'args':args}:
-            return screept.ExprFuncCall(parse_identifier(identifier),list(map(parse_expression,args)))
-        case {'type':'parens', 'expression':expr}:
-            return  parse_expression(expr)
+        case {'type': 'fun_call', 'identifier': identifier, 'args': args}:
+            return screept.ExprFuncCall(parse_identifier(identifier), list(map(parse_expression, args)))
+        case {'type': 'parens', 'expression': expr}:
+            return parse_expression(expr)
         # case
         case _:
             pprint(d)
-            raise Exception("EXPR",d)
+            raise Exception("EXPR", d)
 
+
+def parse_statement(x):
+    match (x):
+        case {'type': 'block', 'statements': stmts}:
+            return screept.StmtBlock(list(map(parse_statement, stmts)))
+        case {'type': 'bind', 'identifier': identifier, 'value': value}:
+            return screept.StmtBind(parse_identifier(identifier), parse_expression(value))
+        case {'type': 'proc_run', 'identifier': identifier, 'args': args}:
+            return screept.StmtProcRun(parse_identifier(identifier), list(map(parse_expression, args)))
+        case _:
+            pprint(x)
+            raise "CAN't"
 
 
 def process_var(v):
-    name,value = v
-    return (name,parse_value(value))
+    name, value = v
+    return (name, parse_value(value))
+
+
+def process_procedure(v):
+    name, value = v
+    return (name, parse_statement(value))
+
 
 def load_game(title: str):
     with open("data/" + title + ".json", "r") as f:
         data = json.load(f)
-        game_state=data['gameState']
-        env=game_state['screeptEnv']
-        vars=(dict(map(process_var,env['vars'].items())))
-        pprint(vars)
+        game_state = data['gameState']
+        env = game_state['screeptEnv']
+        print(env.keys())
+        vars = (dict(map(process_var, env['vars'].items())))
+        procedures = (dict(map(process_procedure, env['procedures'].items())))
+        # pprint(procedures)
+        environment: screept.Environment = screept.Environment(vars, procedures, [])
+        pprint(environment)
         return data
 
 
