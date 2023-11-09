c958953a61b03578ab42d996419a4b31892b0abc
Jan Skowronski <crianonim@gmail.com>
2023-11-09 13:56:58 +0000

    more playing with consumers/producers

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,12 +1,15 @@
+import json
+from pprint import pprint
+
 from confluent_kafka import Consumer
 
 c = Consumer({
     'bootstrap.servers': 'localhost:29092',
-    'group.id': 'mygroup',
+    'group.id': 'mygroup9',
     'auto.offset.reset': 'earliest'
 })
 
-c.subscribe(['testowy'])
+c.subscribe(['trzeci'])
 
 while True:
     msg = c.poll(1.0)
@@ -18,5 +21,16 @@ while True:
         continue
 
     print('Received message: {}'.format(msg.value().decode('utf-8')))
+    pprint(msg)
+    print("TYPE :",type(msg))
+    pprint(dir(msg))
+    print("headers")
+    pprint(msg.headers())
+    print("value")
+    value = msg.value()
+    pprint(value)
+    print("TYPE :", type(value))
+    pprint(dir(value))
+    print(value.decode('utf-8'))
 
 c.close()
--- kafka_producer.py
+++ kafka_producer.py
@@ -12,14 +12,14 @@ def delivery_report(err, msg):
         print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
 
-for data in ['Pierwsza', 'Druga']:
+for data in [('Pierwsza wiadomość2','w1'), ('Druga wiadomość2','w2'),('Trzecia wiadomość2','w2')]:
     # Trigger any available delivery report callbacks from previous produce() calls
     p.poll(0)
-
+    value,key = data
     # Asynchronously produce a message. The delivery report callback will
     # be triggered from the call to poll() above, or flush() below, when the
     # message has been successfully delivered or failed permanently.
-    p.produce('mytopic', data.encode('utf-8'), callback=delivery_report)
+    p.produce(topic='trzeci',value= value.encode('utf-8'),key=key.encode('utf-8'), callback=delivery_report)
 
 # Wait for any outstanding messages to be delivered and delivery report
 # callbacks to be triggered.
