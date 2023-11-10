43f2d3a806658606f0d9b83e3ca85b9fe90aeeca
Jan Skowronski <crianonim@gmail.com>
2023-11-10 12:01:46 +0000

    parametrised consumer invocation

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,26 +1,12 @@
 import json
+import sys
 from pprint import pprint
 
 from confluent_kafka import Consumer
 
-c = Consumer({
-    'bootstrap.servers': 'localhost:29092',
-    'group.id': 'mygroup9',
-    'auto.offset.reset': 'earliest'
-})
 
-c.subscribe(['trzeci'])
 
-while True:
-    msg = c.poll(1.0)
 
-    if msg is None:
-        continue
-    if msg.error():
-        print("Consumer error: {}".format(msg.error()))
-        continue
-
-    print('Received message: {}'.format(msg.value().decode('utf-8')))
     # pprint(msg)
     # print("TYPE :",type(msg))
     # pprint(dir(msg))
@@ -33,5 +19,28 @@ while True:
     # pprint(dir(value))
     # print(value.decode('utf-8'))
 
+def consume_as_group(group:str="mygroup9"):
+    c = Consumer({
+        'bootstrap.servers': 'localhost:29092',
+        'group.id': group,
+        'auto.offset.reset': 'earliest'
+    })
 
-c.close()
+    c.subscribe(['trzeci'])
+    while True:
+        msg = c.poll(5.0)
+
+        if msg is None:
+            continue
+        if msg.error():
+            print("Consumer error: {}".format(msg.error()))
+            continue
+
+        print('Received message: {}'.format(msg.value().decode('utf-8')))
+        print(f' at partition {msg.partition()}')
+
+    c.close()
+
+if __name__ == '__main__':
+    group = "mygroup9" if len(sys.argv)<2 else sys.argv[1]
+    consume_as_group(group)
